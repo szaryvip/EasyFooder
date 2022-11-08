72d6f9ee886616018926b4623dca5c187cfda207
Szymon Skarzyński <32488427+Cat107@users.noreply.github.com>
2022-11-09 00:04:47 +0100

    test_app displays all record from "Teachers" table

--- src/Django/EasyFooder/test_app/views.py
+++ src/Django/EasyFooder/test_app/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Teachers
 
 import datetime
 
 def index(request):
-    return HttpResponse(datetime.datetime.now())
+    output = str(datetime.datetime.now())
+    teachers = Teachers.objects.all().values()
+    for teacher in teachers:
+        name = teacher["name"]
+        age = teacher["age"]
+        output += "<br>"
+        output += f"name: {name} | age: {age}"
+    return HttpResponse(output)
