d50610fc5bafb82c893076a0579a3b0d227e6a65
Szarejko Łukasz <lukaszszarejko@wp.pl>
2022-11-08 19:28:07 +0100

    Preapre Django for Github actions

--- src/Django/EasyFooder/EasyFooder/settings.py
+++ src/Django/EasyFooder/EasyFooder/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 import environ
 
@@ -88,6 +89,17 @@ DATABASES = {
         'PORT': env("DB_PORT"),
     }
 }
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
