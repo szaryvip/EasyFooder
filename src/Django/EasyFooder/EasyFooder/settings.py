f9d527fd4e78b703b4e62e61be4ea2a73c452221
Szarejko Łukasz <lukaszszarejko@wp.pl>
2022-11-09 11:23:11 +0100

    Change db engine in github workflow

--- src/Django/EasyFooder/EasyFooder/settings.py
+++ src/Django/EasyFooder/EasyFooder/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
 if os.environ.get('GITHUB_WORKFLOW'):
     DATABASES = {
         'default': {
-           'ENGINE': 'django.db.backends.postgresql',
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
            'NAME': 'github_actions',
            'USER': 'postgres',
            'PASSWORD': 'postgres',
