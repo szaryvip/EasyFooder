42a904f0532da82c9b785d3e2fcb199c5ada7640
Cat107 <rosomak909@gmail.com>
2022-11-09 18:09:09 +0100

    removing old code

--- src/Django/EasyFooder/EasyFooder/settings.py
+++ src/Django/EasyFooder/EasyFooder/settings.py
@@ -142,3 +142,25 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'console': {
+#             'class': 'logging.StreamHandler',
+#         },
+#         'file': {
+#             'level': 'INFO',
+#             'class': 'logging.FileHandler',
+#             'filename': '~/Desktop/debug.log',
+#         },
+#     },
+#     'loggers': {
+#         'django': {
+#             'handlers': ['file'],
+#             'level': 'INFO',
+#             # 'propagate': True,
+#         },
+#     },
+# }
