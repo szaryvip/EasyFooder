8f85cbdf48e67c731909190eea0e6466c88133eb
Cat107 <rosomak909@gmail.com>
2022-11-26 20:17:25 +0100

    models fix

--- scripts/db_reset.sh
+++ scripts/db_reset.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 sudo -u postgres psql -c "DROP TABLE test_app_teacher"
+sudo -u postgres psql -c "DROP TABLE test_app_meal_tag"
+sudo -u postgres psql -c "DROP TABLE test_app_order"
+sudo -u postgres psql -c "DROP TABLE test_app_meal"
+sudo -u postgres psql -c "DROP TABLE test_app_tag"
+sudo -u postgres psql -c "DROP TABLE test_app_user"
+
+rm ./src/Django/EasyFooder/test_app/migrations/0*
 
 cd ./src/Django/EasyFooder
-python3 manage.py migrate --fake test_app zero
 
 python3 manage.py makemigrations
+python3 manage.py migrate --fake test_app zero
 python3 manage.py migrate
+
+# SELECT * table_name FROM information_schema.tables WHERE table_name LIKE 'test_app%';
--- src/Django/EasyFooder/test_app/models.py
+++ src/Django/EasyFooder/test_app/models.py
@@ -3,6 +3,41 @@ from django.db import models
 # Create your models here.
 
 
+# Sztuczna klasa:
 class Teacher(models.Model):
     name = models.CharField(max_length=80)
     age = models.IntegerField()
+
+# Prawdziwe klasy:
+class Tag(models.Model):
+    tag_id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+
+class Meal(models.Model):
+    meal_id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+
+    tags = models.ManyToManyField(
+        Tag,
+        through='Meal_Tag'
+    )
+
+class Meal_Tag(models.Model):
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+class User(models.Model):
+    user_id = models.BigAutoField(primary_key=True)
+    login = models.CharField(max_length=30)
+    password = models.CharField(max_length=100)
+
+    orders = models.ManyToManyField(
+        Meal,
+        through='Order'
+    )
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
+
+    date = models.DateField()
