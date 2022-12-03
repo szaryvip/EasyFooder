aa2243a5d556dcb03dad1498997715b5cae82fb7
Cat107 <rosomak909@gmail.com>
2022-12-03 16:38:10 +0100

    updated db_reset script

--- scripts/db_reset.sh
+++ scripts/db_reset.sh
@@ -1,30 +1,19 @@
 #!/bin/bash
 
-# The following script resets migrations for test_app
+sudo -u postgres -i psql -c "DROP TABLE test_app_teacher"
+sudo -u postgres -i psql -c "DROP TABLE test_app_meal_tag"
+sudo -u postgres -i psql -c "DROP TABLE test_app_order"
+sudo -u postgres -i psql -c "DROP TABLE test_app_meal"
+sudo -u postgres -i psql -c "DROP TABLE test_app_tag"
+sudo -u postgres -i psql -c "DROP TABLE test_app_user"
 
-# Go to proper directory
 cd ./src/Django/EasyFooder/
 
-# Create new migrations
-python3 manage.py makemigrations
-
-# Clear the migration history
 python3 manage.py migrate --fake test_app zero
 
-# Remove migration files
-rm test_app/migrations/0*
+find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find . -path "*/migrations/*.pyc"  -delete
 
-# Create the initial migrations
 python3 manage.py makemigrations
 
-# Fake the initial migration
 python3 manage.py migrate --fake-initial
-
-# inspiration: https://simpleisbetterthancomplex.com/tutorial/2016/07/26/how-to-reset-migrations.html
-
-# sudo -u postgres -i psql -c "DROP TABLE test_app_teacher"
-# sudo -u postgres -i psql -c "DROP TABLE test_app_meal_tag"
-# sudo -u postgres -i psql -c "DROP TABLE test_app_order"
-# sudo -u postgres -i psql -c "DROP TABLE test_app_meal"
-# sudo -u postgres -i psql -c "DROP TABLE test_app_tag"
-# sudo -u postgres -i psql -c "DROP TABLE test_app_user"
