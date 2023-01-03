da971dcf8d87bb4f55bba26133a3f6939e3c98c6
Szymon Skarzyński <rosomak909@gmail.com>
2023-01-03 22:38:59 +0100

    orders endpoint

--- src/Django/EasyFooder/Users/views.py
+++ src/Django/EasyFooder/Users/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Order
+from Meals.models import Meal
 
-def index(request):
-    return HttpResponse("Hello! You are in users module.")
+def members(request):
+  orders = Order.objects.all().values()
+  meals = Meal.objects.all().values()
+
+  template = loader.get_template('orders.html')
+  context = {
+    'orders': orders,
+    'meals': meals
+  }
+
+  return HttpResponse(template.render(context, request))
