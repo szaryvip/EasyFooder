97cf355dc9617addfad011d64af3bab3c92a288e
Szarejko Łukasz <lukaszszarejko@wp.pl>
2023-01-04 11:19:15 +0100

    SQL seeds

---
+++ pis-dev.session.sql
@@ -0,0 +1,87 @@
+-- DROP TABLE "Meals_meal_tag";
+-- DROP TABLE "Users_order";
+-- DROP TABLE "Meals_meal";
+-- DROP TABLE "Meals_tag";
+-- DROP TABLE "Users_user";
+
+-- seedowanie posiłków
+INSERT INTO "Meals_meal" (meal_id, name, price)
+VALUES (
+    '1',
+    'Kebab',
+    15
+  );
+
+INSERT INTO "Meals_meal" (meal_id, name, price)
+VALUES (
+    '2',
+    'Burger',
+    20
+  );
+
+INSERT INTO "Meals_meal" (meal_id, name, price)
+VALUES (
+    '3',
+    'Pizza',
+    30
+  );
+
+INSERT INTO "Meals_meal" (meal_id, name, price)
+VALUES (
+    '4',
+    'Frytki',
+    5
+  );
+
+INSERT INTO "Meals_meal" (meal_id, name, price)
+VALUES (
+    '5',
+    'Sałatka',
+    20
+  );
+
+-- seedowanie tagow
+INSERT INTO "Meals_tag" (tag_id, name)
+VALUES (
+    '1',
+    'pikantne'
+  );
+
+INSERT INTO "Meals_tag" (tag_id, name)
+VALUES (
+    '2',
+    'zdrowe'
+  );
+
+-- seedowanie powiazan tagow z posilkami
+INSERT INTO "Meals_meal_tag" (id, meal_id_id, tag_id_id)
+VALUES (
+    '1',
+    '1',
+    '1'
+  );
+
+INSERT INTO "Meals_meal_tag" (id, meal_id_id, tag_id_id)
+VALUES (
+    '2',
+    '5',
+    '2'
+  );
+
+-- seed usera testowego
+INSERT INTO "Users_user" (user_id, login, password)
+VALUES (
+    '1',
+    'login:test',
+    'password:test'
+  );
+
+-- seed przykladowego zamowienia
+INSERT INTO "Users_order" (order_id, date, status, meal_id_id, user_id_id)
+VALUES (
+    '1',
+    '02/01/2023',
+    'Dostarczono',
+    '1',
+    '1'
+  );
