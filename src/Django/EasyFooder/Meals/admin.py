0be5492c5b28b6c296a394e1732d20e986a75df4
Kuba Forczek <kuba.forczek@gmail.com>
2023-01-06 19:03:55 +0100

    Changed Meal_tag model; added admin sections for Meal, Tag, Meal_tag

--- src/Django/EasyFooder/Meals/admin.py
+++ src/Django/EasyFooder/Meals/admin.py
@@ -1,11 +1,41 @@
 from django.contrib import admin
 
-from .models import Meal
+from .models import Meal, Tag, Meal_tag, Order
 
 
 class MealAdmin(admin.ModelAdmin):
     model = Meal
-    list_display = ('meal_id', 'name', 'price')
+    list_display = ('name', 'price', 'meal_id', 'get_tags')
+
+    def get_tags(self, obj: Meal):
+        meal_tags = Meal_tag.objects.filter(meal_id=obj.meal_id)
+        tags_print = "".join([f"{meal_tag.tag.name}, " for meal_tag in meal_tags])
+        return tags_print[:-2] if tags_print else tags_print
+    get_tags.short_description = "Tags"
+
+
+class TagAdmin(admin.ModelAdmin):
+    model = Tag
+    list_display = ('name', 'tag_id', 'get_meal_count')
+
+    def get_meal_count(self, obj: Tag):
+        return Meal_tag.objects.filter(tag_id=obj.tag_id).count()
+    get_meal_count.short_description = "Meal count"
+
+
+class Meal_TagAdmin(admin.ModelAdmin):
+    model = Meal_tag
+    list_display = ('get_meal_name', 'meal_id', 'get_tag_name', 'tag_id')
+
+    def get_meal_name(self, obj: Meal_tag):
+        return Meal.objects.get(meal_id=obj.meal_id).name
+    get_meal_name.short_description = "Meal name"
+
+    def get_tag_name(self, obj: Meal_tag):
+        return Tag.objects.get(tag_id=obj.tag_id).name
+    get_tag_name.short_description = "Tag name"
 
 
 admin.site.register(Meal, MealAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Meal_tag, Meal_TagAdmin)
--- src/Django/EasyFooder/Meals/models.py
+++ src/Django/EasyFooder/Meals/models.py
@@ -7,15 +7,21 @@ class Meal(models.Model):
     name = models.CharField(max_length=60)
     price = models.DecimalField(decimal_places=2, max_digits=6)
 
+    def __repr__(self):
+        return f"Meal: {self.name}"
+
 
 class Tag(models.Model):
     tag_id = models.BigIntegerField(primary_key=True)
     name = models.CharField(max_length=15)
 
+    def __repr__(self):
+        return f"TEST: {self.name}"
+
 
 class Meal_tag(models.Model):
-    tag_id = models.ForeignKey(Tag, on_delete=models.CASCADE)
-    meal_id = models.ForeignKey(Meal, on_delete=models.CASCADE)
+    tag = models.ForeignKey('Meals.Tag', on_delete=models.CASCADE)
+    meal = models.ForeignKey('Meals.Meal', on_delete=models.CASCADE)
 
 
 class Order(models.Model):
