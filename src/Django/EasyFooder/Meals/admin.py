e68bd6e75ed7a89d7625a4156ef0d7905617b871
Kuba Forczek <kuba.forczek@gmail.com>
2023-01-06 16:48:38 +0100

    Admin for Meal model

--- src/Django/EasyFooder/Meals/admin.py
+++ src/Django/EasyFooder/Meals/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Meal
+
+
+class MealAdmin(admin.ModelAdmin):
+    model = Meal
+    list_display = ('meal_id', 'name', 'price')
+
+
+admin.site.register(Meal, MealAdmin)
